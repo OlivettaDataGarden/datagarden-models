ee6a436b9b0969276278666e68995209af71d3e6
Maarten de Ruyter <devops@geodatagarden.com>
2024-07-27 21:56:27 +0200

    added first version of economics model

--- src/datagarden_models/models/__init__.py
+++ src/datagarden_models/models/__init__.py
@@ -1,9 +1,11 @@
 from .base import DataGardenModel
 from .demographics import DemographicsV1, DemographicsV1Keys
+from .economics import EconomicsV1, EconomicsV1Keys
 
 
 class DefaultAppModels:
 	DEMOGRAPHICS: type[DataGardenModel] = DemographicsV1
+	ECOMOMICS: type[DataGardenModel] = EconomicsV1
 
 
-__all__ = ["DemographicsV1Keys", "DemographicsV1", "DefaultAppModels"]
+__all__ = ["DemographicsV1Keys", "EconomicsV1Keys", "DefaultAppModels"]
