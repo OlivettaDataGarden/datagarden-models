23c92b61b349b537a7c346324c142b530199bc7f
Maarten de Ruyter <devops@geodatagarden.com>
2024-11-04 18:02:54 +0100

    fixed linting issue

--- src/datagarden_models/models/demographics/base_demographics.py
+++ src/datagarden_models/models/demographics/base_demographics.py
@@ -22,4 +22,6 @@ L = DemographicsBaseLegends
 
 class AgeGender(DataGardenSubModel):
 	male: dict[str, int] = Field(default_factory=dict, description=L.AGE_GENDER_MALE)
-	female: dict[str, int] = Field(default_factory=dict, description=L.AGE_GENDER_FEMALE)
+	female: dict[str, int] = Field(
+		default_factory=dict, description=L.AGE_GENDER_FEMALE
+	)
