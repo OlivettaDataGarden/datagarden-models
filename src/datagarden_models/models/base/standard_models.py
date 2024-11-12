626e6bffda128df3d2d4047374dec91bd2da30bd
Maarten de Ruyter <devops@geodatagarden.com>
2024-11-12 16:53:19 +0100

    update standard models

--- src/datagarden_models/models/base/standard_models.py
+++ src/datagarden_models/models/base/standard_models.py
@@ -5,6 +5,10 @@ from pydantic import Field
 from .models import DataGardenSubModel
 
 
+###########################################
+########## Start Model defenition #########
+###########################################
+
 class EconomicBaseKeys:
 	VALUE = "value"
 	UNIT = "unit"
@@ -25,23 +29,35 @@ class EconomicsValue(DataGardenSubModel):
 	unit: int = Field(default=1, description=L.UNIT)
 	currency: str = Field(default="EUR", description=L.CURRENCY)
 
+	class Meta:
+		exclude_fields_in_has_values_check: list[str] = ['unit', 'currency']
 
+###########################################
+########## Start Model defenition #########
+###########################################
 class ValueAndPercentageKeys:
 	VALUE = "value"
 	PERCENTAGE = "percentage"
 	UNIT = "unit"
+	COUNT = "count"
 
-
-class ValueAndPercentageLegends:
+class CountValueAndPercentageLegends:
 	VALUE = "value in units of given currency,"
 	PERCENTAGE = "percentage of total."
 	UNIT = "units of measure."
+	COUNT = "count of items in single units."
 
 
-VP = ValueAndPercentageLegends
+VP = CountValueAndPercentageLegends
 
 
 class ValueAndPercentage(DataGardenSubModel):
 	value: Optional[float] = Field(default=None, description=VP.VALUE)
 	percentage: Optional[float] = Field(default=None, description=VP.PERCENTAGE)
 	unit: int = Field(default=1, description=VP.UNIT)
+	count: Optional[float] = Field(default=None, description=VP.COUNT)
+
+	class Meta:
+		exclude_fields_in_has_values_check: list[str] = ['unit']
+
+	
